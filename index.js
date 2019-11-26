6175bb91f5b1000951e117d5aba3d94b3e2ff871
edbutcher <edbutcher200@gmail.com>
2019-11-26 09:59:41 +0200

    [Introduction] Task 1.1 (#1)

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log('Works!');
+require('./introduction/task1')();
---
+++ introduction/task1.js
@@ -0,0 +1,21 @@
+function reverseSTDIN() {
+  process.stdin.setEncoding('utf8');
+  console.log('Enter something here to reverse it:');
+
+  process.stdin.on('readable', () => {
+    let chunk;
+
+    while ((chunk = process.stdin.read()) !== null) {
+      process.stdout.write(
+        chunk
+          .slice(0, chunk.length - 1)
+          .split(/(?:)/u)
+          .reverse()
+          .join('')
+          + '\n\n'
+      );
+    };
+  });
+};
+
+module.exports = reverseSTDIN;
