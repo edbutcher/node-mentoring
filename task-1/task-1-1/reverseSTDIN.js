9e5e51164e25b8c1c95faaadbbe518a95d4c7280
Oleksandr Poliakov <oleksandr_poliakov@epam.com>
2019-12-07 14:40:16 +0200

    Fix ends of lines

--- task-1/task-1-1/reverseSTDIN.js
+++ task-1/task-1-1/reverseSTDIN.js
@@ -1,6 +1,6 @@
 function reverseSTDIN() {
-  process.stdin.setEncoding('utf8');
-  console.log('Enter something here to reverse it:');
+  process.stdin.setEncoding('utf8')
+  console.log('Enter something here to reverse it:')
 
   process.stdin.on('readable', () => {
     let chunk;
@@ -13,9 +13,9 @@ function reverseSTDIN() {
           .reverse()
           .join('')
           + '\n\n'
-      );
-    };
+      )
+    }
   });
 };
 
-export default reverseSTDIN;
+export default reverseSTDIN
